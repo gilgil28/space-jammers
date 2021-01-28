4404421bff29dd470912aa85f9b3577d14cc3a74
DESKTOP-CJKAATR\gilgil <gilgil28@gmail.com>
2021-01-28 20:54:38 +0200

    Added player movement script

---
+++ Assets/Scripts/ThirdPersonCameraControl.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class ThirdPersonCameraControl : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private Transform _player;
+
+    [SerializeField] private float _rotationSpeed = 1;
+    [SerializeField] private float _zoomSpeed = 2;
+
+    private Transform _obstruction;
+    private float _mouseX, _mouseY;
+
+
+    private void Start()
+    {
+        _obstruction = _target;
+        // Cursor.visible = false;
+        // Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void LateUpdate()
+    {
+        CamControl();
+        ViewObstructed();
+    }
+
+
+    private void CamControl()
+    {
+        _mouseX += Input.GetAxis("Mouse X") * _rotationSpeed;
+        _mouseY -= Input.GetAxis("Mouse Y") * _rotationSpeed;
+        _mouseY = Mathf.Clamp(_mouseY, -25, 60);
+
+        transform.LookAt(_target);
+        
+        _target.rotation = Quaternion.Euler(_mouseY, _mouseX, 0);
+        _player.rotation = Quaternion.Euler(0, _mouseX, 0);
+    }
+
+
+    private void ViewObstructed()
+    {
+        if (Physics.Raycast(transform.position, _target.position - transform.position, out var hit, 4.5f))
+        {
+            if (!hit.collider.gameObject.CompareTag("Player"))
+            {
+                _obstruction = hit.transform;
+                _obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+                
+                if(Vector3.Distance(_obstruction.position, transform.position) >= 3f && Vector3.Distance(transform.position, _target.position) >= 1.5f)
+                    transform.Translate(Vector3.forward * _zoomSpeed * Time.deltaTime);
+            }
+            else
+            {
+                _obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                if (Vector3.Distance(transform.position, _target.position) < 4.5f)
+                    transform.Translate(Vector3.back * _zoomSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCharacterControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ThirdPersonCharacterControl : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5;
+
+    private void Update ()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        var hor = Input.GetAxis("Horizontal");
+        var ver = Input.GetAxis("Vertical");
+        var run = Input.GetKey(KeyCode.LeftShift) ? 2 : 1;
+
+        Debug.Log(run);
+        var playerMovement = new Vector3(hor, 0f, ver) * (_speed * run * Time.deltaTime);
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
