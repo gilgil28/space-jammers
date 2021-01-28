e5c0017996e42e8734a1556c838d94b8e5293aae
DESKTOP-CJKAATR\gilgil <gilgil28@gmail.com>
2021-01-29 00:27:51 +0200

    jump is optional

--- Assets/Scripts/ThirdPersonCharacterControl.cs
+++ Assets/Scripts/ThirdPersonCharacterControl.cs
@@ -1,8 +1,20 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class ThirdPersonCharacterControl : MonoBehaviour
 {
     [SerializeField] private float _speed = 5;
+
+    [SerializeField] private float _jumpForce = 5;
+    [SerializeField] private bool _allowJump = true;
+
+    private int _touchingColliders;
+    private Rigidbody _rigidBody;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
 
     private void Update ()
     {
@@ -14,9 +26,25 @@ public class ThirdPersonCharacterControl : MonoBehaviour
         var hor = Input.GetAxis("Horizontal");
         var ver = Input.GetAxis("Vertical");
         var run = Input.GetKey(KeyCode.LeftShift) ? 2 : 1;
-
-        Debug.Log(run);
-        var playerMovement = new Vector3(hor, 0f, ver) * (_speed * run * Time.deltaTime);
+        
+        if (_allowJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_touchingColliders > 0)
+            {
+                _rigidBody.AddForce(0, _jumpForce, 0, ForceMode.Impulse);
+            }
+        }
+        var playerMovement = new Vector3(hor, 0f, ver).normalized * (_speed * run * Time.deltaTime);
         transform.Translate(playerMovement, Space.Self);
+    }
+
+    private void OnCollisionEnter ()
+    {
+        _touchingColliders++;
+    }
+
+    private void OnCollisionExit ()
+    {
+        _touchingColliders--;
     }
 }
