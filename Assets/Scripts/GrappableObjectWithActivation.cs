4ddfe9fed190d480925feaf2044c274b2f4b3a80
DESKTOP-69B688E\user <stav12212@gmail.com>
2021-01-30 02:11:39 +0200

    Changed Grappable

---
+++ Assets/Scripts/GrappableObjectWithActivation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+
+
+
+    class GrappableObjectWithActivation : GrappableObject
+    {
+
+        [SerializeField] GameObject mObjectToActivate;
+
+
+        public override void OnGrapped()
+        {
+            if(!mObjectToActivate)
+            {
+                throw new Exception("Object must have an activation object");
+            }
+            mObjectToActivate.GetComponent<GrappableObject>().SetType(GrappleType.Heavy);
+        }
+    }
+}
