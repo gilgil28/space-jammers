c411bf4a8836132de4aff904ad3ca06993174d01
DESKTOP-CJKAATR\gilgil <gilgil28@gmail.com>
2021-01-30 02:28:59 +0200

    battery logic

---
+++ Assets/Scripts/FlashlightAbilityController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FlashlightAbilityController : MonoBehaviour
+{
+    private GameObject _flashlight;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.LeftAlt))
+        {
+            _flashlight.SetActive(!_flashlight.activeSelf);
+        }
+    }
+
+    public void SetFlashlightGameObject(GameObject flashlight)
+    {
+        _flashlight = flashlight;
+    }
+}
